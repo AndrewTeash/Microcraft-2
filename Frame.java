6fa3b03617164d53b14ec3129467344ea20f9285
AndrewTeash <96268071+AndrewTeash@users.noreply.github.com>
2022-11-11 22:24:56 -0600

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,12 @@
+package Sources.Window;
+import java.awt.*;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+	public static boolean Running = true;
+	public Frame() {
+		this.setVisible(true);
+		this.setFocusable(true);
+	}
+}
